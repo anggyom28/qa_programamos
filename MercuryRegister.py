e66c6f0027502be331d36653ab2fd4261c1ea270
Anggy Orozco <anggy0428@gmail.com>
2024-05-21 20:12:05 -0500

    Mejora del codigo(try except)

--- MercuryRegister.py
+++ MercuryRegister.py
@@ -1,28 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://demo.guru99.com/test/newtours/")
+driver.get("https://demo.guru99.com/test/newtours/index.php")
 driver.maximize_window()
 driver.execute_script("window.scrollTo(0,500)")
-
-
-button = driver.find_element(By.LINK_TEXT,"REGISTER") .click()
 time.sleep(2)
 
-email = driver.find_element(By.XPATH, "//input[contains(@id,'email')]") .send_keys("anggy0428@gmail.com")
-time.sleep(2)
+buttonRegister = driver.find_element(By.LINK_TEXT, "REGISTER") .click()
+time.sleep(1)
 
-password = driver.find_element(By.XPATH, "//input[contains(@name,'password')]") .send_keys("a123456")
-time.sleep(2)
+try:
+    registerImag = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//img[@src='images/mast_register.gif']"))
+        )
+      
+    if registerImag:
+        print("!Bienvenido a la página del registro¡")
+        print("Por favor procede a llenar los campos")
 
-confirmPassword = driver.find_element(By.XPATH, "//input[contains(@name,'confirmPassword')]") .send_keys("a123456")
-time.sleep(2)
+    mail = driver.find_element(By.XPATH, "//input[contains(@id,'email')]") .send_keys("anggy04@gmail.com")
+    time.sleep(1)
+    
+    password = driver.find_element(By.XPATH, "//input[contains(@name,'password')]") .send_keys("Prueba.01")
+    time.sleep(1)
+    
+    confirmPassword = driver.find_element(By.XPATH, "//input[contains(@name,'confirmPassword')]") .send_keys("Prueba.01")
+    time.sleep(1)
+    
+    button = driver.find_element(By.XPATH, "//input[@src='images/submit.gif']") .click()
+    time.sleep(2)
+    
+    confirmationMessage = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//b[contains(.,'Note: Your user name is anggy04@gmail.com.')]"))
+        )
+    
+    if confirmationMessage:
+        print("Mensaje de confirmación encontrado")
+        print(confirmationMessage.text)
+except Exception as e:
+    print("Error al cargar la pagina de resgister", e)
 
-button = driver.find_element(By.XPATH,"//input[@src='images/submit.gif']") .click()
-time.sleep(2)
-
-confirmationMessage = driver.find_element(By.XPATH,"//b[contains(.,'Note: Your user name is anggy0428@gmail.com')]")
-driver.close()
+finally:
+    driver.close()
