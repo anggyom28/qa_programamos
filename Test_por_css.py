9137ccd4930bf019b17a631059041e8f6287aab8
Anggy Orozco <anggy0428@gmail.com>
2024-05-21 20:17:09 -0500

    practicando selectores

---
+++ Test_por_css.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/text-box")
+driver.maximize_window()
+
+name = driver.find_element(By.CSS_SELECTOR, "#userName")
+name.send_keys("anggy")
+time.sleep(2)
+
+email = driver.find_element(By.CSS_SELECTOR, "#userEmail") .send_keys("Ang8@gmail.com")
+time.sleep(2)
+
+current_address = driver.find_element(By.CSS_SELECTOR, "#currentAddress") .send_keys("suiza")
+time.sleep(2)
+
+permanent_address = driver.find_element(By.CSS_SELECTOR, "#permanentAddress") .send_keys("cualquier cosa")
+time.sleep(2)
+
+#comando para hacer scroll
+# driver.execute_script("window.scrollTo(0,250")
+# time.sleep(2)
+
+button = driver.find_element(By.CSS_SELECTOR, "#submit") .click()
+time.sleep(2)
+driver.close()
